ad75785dfa7b96dac20493abf36494db8c21781a
mai <mai0abdelaziz@gmail.com>
2014-04-26 11:11:51 +0200

    The second commit for assig2, better comment indents

--- cachematrix.R
+++ cachematrix.R
@@ -9,7 +9,7 @@
 makeCacheMatrix <- function(x = matrix()) {
 
 		i <- NULL   			## i which is the inverse of the matrix
-								## is first initialized to NULL
+							## is first initialized to NULL
         
 		set <- function(y) {	## set x to y's value
                 x <<- y			## we use the <<- operator as x isn't found
@@ -17,12 +17,12 @@ makeCacheMatrix <- function(x = matrix()) {
         }
         get <- function() x		## get the stored value of x
         setinv <- function(inv) i <<- inv	## similar to set function
-											## but this time we set i to
-											## the inverse of the matrix
+							## but this time we set i to
+							## the inverse of the matrix
         
-		getinv <- function() i				## similar to get, but returns i's value
+		getinv <- function() i			## similar to get, but returns i's value
         
-		list(set = set, get = get,			## the returned argument is a list
+		list(set = set, get = get,		## the returned argument is a list
              setinv = setinv,				## that contains the values of the
              getinv = getinv)				## results of all 4 functions
 
@@ -36,15 +36,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-		i <- x$getinv()			## get the value of the inverse 
-        if(!is.null(i)) {		## if cache is found
+		i <- x$getinv()				## get the value of the inverse 
+        if(!is.null(i)) {		      	## if cache is found
                 message("getting cached data")	##type this message
-                return(i)		## and return the cached value
+                return(i)				## and return the cached value
         }
         data <- x$get()			## if cache not found
-        i <- solve(data, ...)	## compute it here
-        x$setinv(i)				## save it to cached values
-        i						## return the inverse as a result
+        i <- solve(data, ...)		## compute it here
+        x$setinv(i)			## save it to cached values
+        i					## return the inverse as a result
 
 
 }
